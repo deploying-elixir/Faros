ca622e136f6bf5490b81d1b824bf64abb00d74f2
Georgina McFadyen <gemcfadyen@hotmail.co.uk>
2017-03-12 16:35:22 +0000

    Upgrading elixir version to 1.4

--- apps/post_service/mix.exs
+++ apps/post_service/mix.exs
@@ -19,7 +19,7 @@ defmodule PostService.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger, :ecto, :db_connection, :postgrex],
+    [extra_applications: [:logger, :ecto, :postgrex],
      mod: {PostService.Application, []}]
   end
 
