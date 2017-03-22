acafc06eaf1dba0ca3039e32e1419bc740bacc5b
Georgina McFadyen <gemcfadyen@hotmail.co.uk>
2017-03-22 20:12:25 +0000

    Skeleton db service added

---
+++ apps/db_service/lib/db_service.ex
@@ -0,0 +1,18 @@
+defmodule DbService do
+  @moduledoc """
+  Documentation for DbService.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DbService.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/db_service/test/db_service_test.exs
@@ -0,0 +1,8 @@
+defmodule DbServiceTest do
+  use ExUnit.Case
+  doctest DbService
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
