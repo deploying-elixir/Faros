530a7a545d1fb9ae3ff4cf0c3ed63861a42b3ff7
Makis Otman <makisotman@gmail.com>
2017-03-24 17:42:33 +0000

    Change store function to use cast instead of call

--- apps/memory_db/lib/memory_db.ex
+++ apps/memory_db/lib/memory_db.ex
@@ -7,11 +7,11 @@ defmodule MemoryDb do
   end
 
   def store(result = %{type: :search}) do
-    GenServer.call(Storage, {:store_search, result})
+    GenServer.cast(Storage, {:store_search, result})
   end
 
   def store(result = %{type: :post}) do
-    GenServer.call(Storage, {:store_post, result})
+    GenServer.cast(Storage, {:store_post, result})
   end
 
   def lookup(query = %{type: :search}) do
@@ -22,18 +22,18 @@ defmodule MemoryDb do
     GenServer.call(Storage, {:lookup_post, query})
   end
 
-  def handle_call({:store_post, result}, _from, state) do
+  def handle_cast({:store_post, result}, state) do
     updated_list = state.post ++ [result]
     new_state = %{state | post: updated_list}
 
-    {:reply, new_state, new_state}
+    {:noreply, new_state}
   end
 
-  def handle_call({:store_search, result}, _from, state) do
+  def handle_cast({:store_search, result}, state) do
     updated_list = state.search ++ [result]
     new_state = %{state | search: updated_list}
 
-    {:reply, new_state, new_state}
+    {:noreply, new_state}
   end
 
   def handle_call({:lookup_search, %{type: :search, source: query_source}}, _from, state) do
