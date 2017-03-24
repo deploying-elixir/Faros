b8bf2fee404572a1acd7d7ea6bd457cd0bb26feb
Makis Otman <makisotman@gmail.com>
2017-03-24 14:15:30 +0000

    In memory DB skeleton

---
+++ apps/memory_db/lib/memory_db.ex
@@ -0,0 +1,18 @@
+defmodule MemoryDb do
+  @moduledoc """
+  Documentation for MemoryDb.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MemoryDb.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/memory_db/test/memory_db_test.exs
@@ -0,0 +1,8 @@
+defmodule MemoryDbTest do
+  use ExUnit.Case
+  doctest MemoryDb
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
