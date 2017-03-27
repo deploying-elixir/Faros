5fd213ee9594489c2ead53e14cb2cbc0a99f0954
Georgina McFadyen <gemcfadyen@hotmail.co.uk>
2017-03-27 13:27:02 +0100

    Refactoring to use zip in transforming results to a struct

--- apps/wikipedia/lib/response.ex
+++ apps/wikipedia/lib/response.ex
@@ -11,29 +11,12 @@ defmodule Response do
   end
 
   defp format(response_details = [names, descriptions, links]) do
-    IO.puts "separate"
-    IO.inspect response_details
-    grouped = group_details(response_details, [])
-    IO.inspect grouped
-    grouped
+    response_details
+    |> Enum.zip
+    |> Enum.map(fn(x) -> create_struct(x) end)
   end
 
-  defp group_details(details = [[head_title|tail_title], [head_desc|tail_desc], [head_link | tail_link]], all_results) do
-    new_res = all_results ++ [process_single_result(details, [])]
-    group_details([tail_title, tail_desc, tail_link], new_res)
-  end
-  defp group_details([[], [], []], all_results) do
-    all_results
-  end
-
-  defp process_single_result(details, single_result) do
-    details
-    |> Enum.map(fn([head | tail]) -> single_result ++ [head] end)
-    |> List.flatten
-    |> create_struct
-  end
-
-  defp create_struct(details = [title, description, link]) do
+  defp create_struct(details = {title, description, link}) do
     IO.inspect details
     %Result{title: title, description: description, link: link}
   end
