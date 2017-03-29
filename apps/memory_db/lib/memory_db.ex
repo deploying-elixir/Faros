7e5aabe70b176af9015d511c0f57fe2f0b3bb0df
Makis Otman <makisotman@gmail.com>
2017-03-29 18:38:19 +0100

    Update MemoryDb and fix tests issues

--- apps/memory_db/lib/memory_db.ex
+++ apps/memory_db/lib/memory_db.ex
@@ -1,53 +1,91 @@
 defmodule MemoryDb do
   use GenServer
 
+  @moduledoc """
+  MemoryDb is just an in memory storage mechanism using a GenServer to persist your data in memory.
+  """
+
+  @doc """
+  Starts the MemoryDb process and returns the process id.
+
+  ## The PID number below will likely be different on your machine.
+
+      iex> MemoryDb.start_link
+      {:ok, #PID<0.153.0>}
+  """
   def start_link do
-    state = %{search: [], post: []}
-    GenServer.start_link(__MODULE__, state, [name: Storage])
+    state = %{}
+    GenServer.start_link(__MODULE__, state, [name: __MODULE__])
   end
 
-  def store(result = %{type: :search}) do
-    GenServer.cast(Storage, {:store_search, result})
+  @doc """
+  Retrieves the results for the given key
+
+  ## Examples
+      iex> MemoryDb.lookup(:search, "pizza")
+      iex> {"pizza", [%{description: "A pizza article"}]}
+
+      iex> results = [%{description: "A pizza article"}]
+      iex> MemoryDb.store("pizza", results)
+      iex> :ok
+
+      iex> MemoryDb.lookup(:search, "pizza")
+      iex> {"pizza", [%{description: "A pizza article"}]}
+  """
+  @spec lookup(atom, String.t) :: {String.t, list(map)}
+  def lookup(:search, topic) do
+    GenServer.call(__MODULE__, {:lookup, topic})
   end
 
-  def store(result = %{type: :post}) do
-    GenServer.cast(Storage, {:store_post, result})
+  @doc """
+  Stores the given results using the topic as a key.
+  If the key exists, it will update the existing record, otherwise it will create a new entry for that key.
+
+  ## Examples
+
+      iex> MemoryDb.lookup(:search, "pizza")
+      iex> {"pizza", :not_found}
+
+      iex> results = [%{description: "A pizza article"}]
+      iex> MemoryDb.store("pizza", results)
+      iex> :ok
+
+      iex> MemoryDb.lookup(:search, "pizza")
+      iex> {"pizza", [%{description: "A pizza article"}]}
+
+      iex> new_results = [%{description: "Another pizza article"}]
+      iex> MemoryDb.store("pizza", new_results)
+      iex> :ok
+
+      iex> MemoryDb.lookup(:search, "pizza")
+      iex> {"pizza", [%{description: "A pizza article"}, %{description: "Another pizza article"}]}
+  """
+  @spec store(String.t, list(map)) :: none
+  def store(topic, results) do
+    GenServer.cast(__MODULE__, {:store, {topic, results}})
   end
 
-  def lookup(query = %{type: :search}) do
-    GenServer.call(Storage, {:lookup_search, query})
-  end
-
-  def lookup(query = %{type: :post}) do
-    GenServer.call(Storage, {:lookup_post, query})
-  end
-
-  def handle_cast({:store_post, result}, state) do
-    updated_list = state.post ++ [result]
-    new_state = %{state | post: updated_list}
+  def handle_cast({:store, {topic, results}}, state) do
+    new_state = Map.get(state, topic)
+    |> create_or_update(topic, results, state)
 
     {:noreply, new_state}
   end
 
-  def handle_cast({:store_search, result}, state) do
-    updated_list = state.search ++ [result]
-    new_state = %{state | search: updated_list}
+  def handle_call({:lookup, topic}, _from, state) do
+    result = Map.fetch(state, topic)
+    |> fetch_result
 
-    {:noreply, new_state}
+    {:reply, {topic, result}, state}
   end
 
-  def handle_call({:lookup_search, %{type: :search, source: query_source}}, _from, state) do
-    results = Enum.filter(state.search, fn(element) ->
-      element.source == query_source
-    end)
-    {:reply, results, state}
-  end
+  defp fetch_result({:ok, result}), do: result
+  defp fetch_result(:error), do: :not_found
 
-  def handle_call({:lookup_search, %{type: :search}}, _from, state) do
-    {:reply, state.search, state}
+  defp create_or_update(records, topic, results, state) when is_list(records) do
+    Map.update(state, topic, [], fn(s) -> s ++ results end)
   end
-
-  def handle_call({:lookup_post, %{type: :post}}, _from, state) do
-    {:reply, state.post, state}
+  defp create_or_update(nil, topic, results, state) do
+    Map.put(state, topic, results)
   end
 end
--- apps/memory_db/test/memory_db_test.exs
+++ apps/memory_db/test/memory_db_test.exs
@@ -1,36 +1,68 @@
 defmodule MemoryDbTest do
   use ExUnit.Case, async: true
-  doctest MemoryDb
 
-  test "stores a search result" do
-    search_result = %{description: "A pizza link", link: "some-link", type: :search, source: :guardian}
+  setup do
+    on_exit fn ->
+      GenServer.stop(MemoryDb)
+      MemoryDb.start_link
+    end
 
-    MemoryDb.store(search_result)
-
-    results = MemoryDb.lookup(%{type: :search, source: :guardian})
-    GenServer.stop(Storage)
-    assert length(results)  == 1
+    :ok
   end
 
-  test "stores multiple results" do
-    search_result = %{description: "A pizza link", link: "some-link", type: :search, source: :guardian}
-    another_search_result = %{description: "Another pizza link", link: "link", type: :search, source: :guardian}
-
-    MemoryDb.store(search_result)
-    MemoryDb.store(another_search_result)
-
-    results = MemoryDb.lookup(%{type: :search, source: :guardian})
-    GenServer.stop(Storage)
-    assert length(results)  == 2
+  test "returns not-found when looking up an unknown term" do
+    assert MemoryDb.lookup(:search, "unknown") == {"unknown", :not_found}
   end
 
-  test "stores a post" do
-    post_result = %{url: "post-url", description: "a-description", type: :post}
+  test "stores a list of results for a single topic" do
+    results = [
+      %{description: "A pizza link", link: "some-link", type: :search, source: :guardian},
+      %{description: "Another pizza link", link: "link", type: :search, source: :guardian}
+    ]
 
-    MemoryDb.store(post_result)
+    MemoryDb.store("pizza", results)
 
-    results = MemoryDb.lookup(%{type: :post})
-    GenServer.stop(Storage)
-    assert length(results)  == 1
+    {"pizza", records} = MemoryDb.lookup(:search, "pizza")
+    assert length(records)  == 2
+  end
+
+  test "stores lists of results for multiple topics" do
+    pizza_results = [
+      %{description: "A pizza link", link: "some-link", source: :guardian},
+      %{description: "Another pizza link", link: "link", source: :guardian}
+    ]
+    cheese_results = [
+      %{description: "A cheese link", link: "some-link", source: :guardian},
+      %{description: "Another cheese link", link: "link", source: :guardian}
+    ]
+
+    MemoryDb.store("pizza", pizza_results)
+    MemoryDb.store("cheese", cheese_results)
+
+    {"pizza", p_records} = MemoryDb.lookup(:search, "pizza")
+    {"cheese", c_records} = MemoryDb.lookup(:search, "cheese")
+    assert length(p_records)  == 2
+    assert length(c_records)  == 2
+    assert %{description: "A pizza link"} = List.first(p_records)
+    assert %{description: "A cheese link"} = List.first(c_records)
+  end
+
+  test "adding results for same topic will return a single list with all" do
+    result_1 = [
+      %{description: "A pizza link", link: "some-link", type: :search, source: :guardian}
+    ]
+    result_2 = [
+      %{description: "Another pizza link", link: "link", type: :search, source: :guardian}
+    ]
+    result_3 = [
+      %{description: "Another pizza link", link: "link", type: :search, source: :guardian}
+    ]
+
+    MemoryDb.store("pizza", result_1)
+    MemoryDb.store("pizza", result_2)
+    MemoryDb.store("pizza", result_3)
+
+    {"pizza", records} = MemoryDb.lookup(:search, "pizza")
+    assert length(records)  == 3
   end
 end
