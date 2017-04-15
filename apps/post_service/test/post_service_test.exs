76138f3d2349e0ab3c18f0004b7ccef79441dc84
Georgina McFadyen <gemcfadyen@hotmail.co.uk>
2017-04-15 12:50:56 +0100

    Adds tests for post serice

--- apps/post_service/test/post_service_test.exs
+++ apps/post_service/test/post_service_test.exs
@@ -1,7 +1,23 @@
 defmodule PostServiceTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  setup do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(DbService.Repo)
+  end
+
+  test "post service persists a post" do
+    post =  PostService.create("a-post", "a-description")
+
+    assert post.name == "a-post"
+    assert post.description == "a-description"
+  end
+
+  test "post service retrieves all posts" do
+    PostService.create("a-post", "a-description")
+    PostService.create("another-post", "another-description")
+
+    all_posts = PostService.all()
+
+    assert length(all_posts) == 2
   end
 end
